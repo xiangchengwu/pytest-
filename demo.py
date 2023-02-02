48d65a298bfc7d500f568dd691a6f2955cb4f5d0
wucheng <wucheng@flyele.net>
2023-02-02 14:14:08 +0800

    first

---
+++ demo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023/02/02 10:36
+@Author  : WuCheng
+@FileName: demo.py
+@Software: PyCharm
+"""
---
+++ pytesting/demo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023/02/02 10:10
+@Author  : WuCheng
+@FileName: demo.py
+@Software: PyCharm
+"""
+hello = 'hello'
